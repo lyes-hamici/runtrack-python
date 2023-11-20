c434e89c97d3b7ecee3911715a7d107d88f757ad
lyes-hamici <146708436+lyes-hamici@users.noreply.github.com>
2023-11-20 13:44:58 +0100

    Ajout d'une deuxième réponse

--- Jour01/Plus loin/Plus loin.py
+++ Jour01/Plus loin/Plus loin.py
@@ -7,3 +7,13 @@ elif "E" in verif:
     
 else:
     print("la lettre 'e' n'est pas présente dans la pharse")
+
+
+#----------------------------------------------------------
+
+verif2 = input("Ecrire une pharse:")
+if "e" in verif2 and "E" in verif2:
+    print("La lettre 'e' est bien présente")
+    
+else:
+    print("la lettre 'e' n'est pas présente dans la pharse")
