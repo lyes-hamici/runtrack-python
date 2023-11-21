0686cd81812931aa039b8ed9f7ae5b574e3af9ea
lyes-hamici <lyes.hamici@laplateforme.io>
2023-11-21 11:46:00 +0100

    Job07

---
+++ Jour02/Job07/Job07.py
@@ -0,0 +1,17 @@
+print("-----------------------------------------")
+print("Boucle For :")
+
+chaine = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+affiche = ""
+for i in chaine:
+    affiche += i
+    print(affiche)
+    print("-----------------------------------------")
+
+print("-----------------------------------------")
+print("-----------------------------------------")
+print("-----------------------------------------")
+
+
+
+
