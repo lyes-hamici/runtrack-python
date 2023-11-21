a58072b7bee8419d3c655e64eceea223096202bc
lyes-hamici <lyes.hamici@laplateforme.io>
2023-11-21 10:13:05 +0100

    Job04 avec deux types de boucles

---
+++ Jour02/Job04/Job04.py
@@ -0,0 +1,18 @@
+print("Boucle For")
+
+a = 10
+
+for i in range(11):
+    print(a * i)
+
+
+
+print("-------------------------------")
+
+print("Boucle While")
+
+b = 5
+j = 0
+while j <= 10:
+    print(b *j)
+    j +=1
