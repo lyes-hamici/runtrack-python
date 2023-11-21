4907318a4b9ea006da4d98fefdd7de036ace87b7
lyes-hamici <lyes.hamici@laplateforme.io>
2023-11-21 10:01:35 +0100

    Job02

---
+++ Jour02/Job02/Job02.py
@@ -0,0 +1,15 @@
+print("Boucle For")
+
+for i in range(0,21,2):
+    print(i)
+
+
+print("-------------------------------")
+
+print("Boucle While")
+
+
+j = 0
+while j <= 20:
+    print(j)
+    j +=2
