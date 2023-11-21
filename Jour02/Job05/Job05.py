6613d7e146037ca342dc24d0d4dbdc06a685cd1c
lyes-hamici <lyes.hamici@laplateforme.io>
2023-11-21 11:12:02 +0100

    Job05 avec deux types de boucles

--- Jour02/Job05/Job05.py
+++ Jour02/Job05/Job05.py
@@ -1,9 +1,52 @@
-print("Boucle For")
+print("Boucle For :")
+
+print("-----------------------------------------")
 
 
 for i in range(101):
-    if i%3:
+
+    if i%5 == 0 and i%3 == 0:
+        print("FizzBuzz")
+
+
+    elif i%3 == 0:
         print("Fizz")
+
+    elif i%5 == 0:
+        print("Buzz")
+
 
     else:
         print(i)
+
+
+
+print("-----------------------------------------")
+
+
+
+
+print("Boucle While :")
+
+j = 0
+while j <=100:
+
+    if j%5 == 0 and j%3 == 0:
+        print("FizzBuzz")
+        j+=1
+
+
+    elif j%3 == 0:
+        print("Fizz")
+        j+=1
+
+    elif j%5 == 0:
+        print("Buzz")
+        j+=1
+
+
+    else:
+        print(j)
+        j+=1
+
+print("-----------------------------------------")
