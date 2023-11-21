ffc47fab4f26ef158bbf644ced9e5ca899381556
lyes-hamici <lyes.hamici@laplateforme.io>
2023-11-21 09:55:40 +0100

    Job01 avec deux type de boucle differentes

--- Jour01/Plus loin/Plus loin.py
+++ Jour01/Plus loin/Plus loin.py
@@ -1,5 +1,9 @@
 verif = input("Ecrire une pharse:")
-if "e" or "E" in verif:
+if "e" in verif:
     print("La lettre 'e' est bien présente")
+
+elif "E" in verif:
+    print("La lettre 'e' est bien présente")
+    
 else:
     print("la lettre 'e' n'est pas présente dans la pharse")
