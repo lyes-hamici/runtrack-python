3813406e5067bca0646b87ab1e1125d232263042
lyes-hamici <lyes.hamici@laplateforme.io>
2023-11-22 11:43:22 +0100

    Correction du Job 09

--- Jour03/Job09/Job09.py
+++ Jour03/Job09/Job09.py
@@ -1,31 +1,31 @@
-def moyenne():
-    note_final = 0
-    a = input("Entrée vôtre première note :")
-    b = input("Entrée vôtre deuxième note :")
-    c = input("Entrée vôtre troisième note :")
+a = input("Entrée vôtre première note :")
+b = input("Entrée vôtre deuxième note :")
+c = input("Entrée vôtre troisième note :")
 
-    note_final = int(a) + int(b) + int(c)
-    print("Note total :",note_final)
+def moyenne(a,b,c):
+    moyenne_etudiant = int(a) + int(b) + int(c)
 
-    moyenne_final = note_final / 3
+    moyenne_etudiant = moyenne_etudiant / 3
 
-    print("Moyenne :",moyenne_final)
+    print("Moyenne :",moyenne_etudiant)
 
 
-    if 15 < moyenne_final <= 20:
+    if 15 <= moyenne_etudiant <= 20:
         print("Très bon élève")
 
-    elif 11 < moyenne_final <= 14:
+    elif 11 <= moyenne_etudiant <= 14:
         print("Bonne éléve")
 
 
-    elif 8 < moyenne_final <= 10:
+    elif 8 <= moyenne_etudiant <= 10:
         print("Élève moyen")
 
 
-    elif 0 < moyenne_final <= 7:
+    elif 0 < moyenne_etudiant <= 7:
         print("Élève devant faire des efforts")
 
-moyenne()
+moyenne(a,b,c)
+
+
 
     
