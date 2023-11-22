345b924a29bc729ada4324554a4862ddba3db761
lyes-hamici <lyes.hamici@laplateforme.io>
2023-11-22 11:03:44 +0100

    Job09

--- Jour03/Job09/Job09.py
+++ Jour03/Job09/Job09.py
@@ -12,6 +12,20 @@ def moyenne():
     print("Moyenne :",moyenne_final)
 
 
+    if 15 < moyenne_final <= 20:
+        print("Très bon élève")
+
+    elif 11 < moyenne_final <= 14:
+        print("Bonne éléve")
+
+
+    elif 8 < moyenne_final <= 10:
+        print("Élève moyen")
+
+
+    elif 0 < moyenne_final <= 7:
+        print("Élève devant faire des efforts")
+
 moyenne()
 
     
