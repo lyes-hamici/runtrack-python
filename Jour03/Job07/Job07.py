913275e2a6451e6360596c9762c36410a9eb7987
lyes-hamici <lyes.hamici@laplateforme.io>
2023-11-22 10:45:47 +0100

    Job07

---
+++ Jour03/Job07/Job07.py
@@ -0,0 +1,83 @@
+def dev(langage):
+    if langage == "JavaScript":
+        print("tu es un développeur web")
+
+    elif langage == "python":
+        print("tu es un développeur IA")
+
+    elif langage == "java":
+        print("tu es un développeur logiciel")
+
+    elif langage == "reactjs":
+        print("tu es un developpeur mobile")
+
+    else:
+        print("un jour, je serai le meilleur développeur... ")
+
+print("---------------------")
+
+dev("JavaScript")
+
+print("---------------------")
+
+dev("python")
+
+print("---------------------")
+
+dev("java")
+
+print("---------------------")
+
+dev("reactjs")
+
+print("---------------------")
+
+dev("test")
+
+print("---------------------")
+
+
+
+
+def dev2(langage):
+    if langage == "JavaScript":
+        return "tu es un développeur web"
+
+    elif langage == "python":
+        return "tu es un développeur IA"
+
+    elif langage == "java":
+        return "tu es un développeur logiciel"
+
+    elif langage == "reactjs":
+        return "tu es un developpeur mobile"
+
+    else:
+        return "un jour, je serai le meilleur développeur... "
+    
+
+
+
+
+
+print("---------------------")
+
+dev("JavaScript")
+
+print("---------------------")
+
+dev("python")
+
+print("---------------------")
+
+dev("java")
+
+print("---------------------")
+
+dev("reactjs")
+
+print("---------------------")
+
+dev("test")
+
+print("---------------------")
