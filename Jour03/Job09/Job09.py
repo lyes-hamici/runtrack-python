8307a340fbb3e98334cbec67be4631f952412a21
lyes-hamici <lyes.hamici@laplateforme.io>
2023-11-22 12:04:37 +0100

    Corretion Job09

--- Jour03/Job09/Job09.py
+++ Jour03/Job09/Job09.py
@@ -3,28 +3,33 @@ b = input("Entrée vôtre deuxième note :")
 c = input("Entrée vôtre troisième note :")
 
 def moyenne(a,b,c):
-    moyenne_etudiant = int(a) + int(b) + int(c)
-
-    moyenne_etudiant = moyenne_etudiant / 3
-
-    print("Moyenne :",moyenne_etudiant)
-
-
-    if 15 <= moyenne_etudiant <= 20:
-        print("Très bon élève")
-
-    elif 11 <= moyenne_etudiant <= 14:
-        print("Bonne éléve")
+    moyenne = float(a) + float(b) + float(c)
+    moyenne = moyenne / 3
+    print("Moyenne :",moyenne)
+    return moyenne
 
 
-    elif 8 <= moyenne_etudiant <= 10:
-        print("Élève moyen")
-
-
-    elif 0 < moyenne_etudiant <= 7:
-        print("Élève devant faire des efforts")
 
 moyenne(a,b,c)
+
+
+moyenne_etudiant = moyenne(a,b,c)
+
+if 15 <= moyenne_etudiant <= 20:
+    print("Très bon élève")
+
+elif 11 <= moyenne_etudiant < 15:
+    print("Bonne éléve")
+
+
+elif 8 <= moyenne_etudiant < 11:
+    print("Élève moyen")
+
+
+elif 0 < moyenne_etudiant <= 7:
+    print("Élève devant faire des efforts")
+
+
 
 
 
