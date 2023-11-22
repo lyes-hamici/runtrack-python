a20b0e5ec3aa8db725362da4af0b294e1d28cb15
lyes-hamici <lyes.hamici@laplateforme.io>
2023-11-22 09:59:59 +0100

    Job02

---
+++ Jour03/Job02/Job02.py
@@ -0,0 +1,16 @@
+def My_print_name(name):
+    return name
+
+print("-----------------------")
+
+print(My_print_name("Lyes"))
+
+print("-----------------------")
+
+print(My_print_name("Than"))
+
+print("-----------------------")
+
+print(My_print_name("Julian"))
+
+print("-----------------------")
