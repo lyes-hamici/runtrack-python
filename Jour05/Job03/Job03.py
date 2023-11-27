125d48efdb42bfb9a9cc51d03879bdb58fc21940
lyes-hamici <lyes.hamici@laplateforme.io>
2023-11-27 10:31:29 +0100

    Job03

--- Jour05/Job03/Job03.py
+++ Jour05/Job03/Job03.py
@@ -4,13 +4,13 @@ def triangle(n):
     for i in range(n):
         vide = ' ' * (n - i - 1)
         if i == 0:
-            print(vide + '_')
+            print(vide + '/\\')
 
         elif i == (n-1):
-            print(vide + '/' + '_' * (2 * i - 1) + '\\')
+            print(vide + '/' + '_' * (2 * i) + '\\')
 
         else:
-            print(vide + '/' + ' ' * (2 * i - 1) + '\\')
+            print(vide + '/' + ' ' * (2 * i) + '\\')
     
 
 
