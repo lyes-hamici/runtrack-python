8121d48a916da1654ebbe5dd0cdef88878be1489
lyes-hamici <lyes.hamici@laplateforme.io>
2023-11-27 15:01:25 +0100

    Correction Job06

--- Jour05/Job06/Job06.py
+++ Jour05/Job06/Job06.py
@@ -1,13 +1,14 @@
 def gardien(nb_marche,cm_marche):
     resulte = 0
-    for i in range(6):
-        resulte += nb_marche * cm_marche
+    resulte = nb_marche * cm_marche
+
+    resulte= resulte*5
 
     resulte_aller_retour = resulte * 2 
 
     resulte_semaine = resulte_aller_retour * 7
 
-    resulte_m_semaine = resulte_semaine * 100
+    resulte_m_semaine = resulte_semaine / 100
 
     print("Pour",nb_marche,"marches de", cm_marche, "cm, le gardien parcourt",resulte_m_semaine,"m par semaine.")
 
